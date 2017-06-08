530acf28adc94206c97eecccd91716714cea5771
Matthias Laug <matthias.laug@gmail.com>
2017-06-08 08:23:18 +0200

    added no authentication for OPTIONS

--- src/main/java/com/thoughtworks/configuration/SecurityConfiguration.java
+++ src/main/java/com/thoughtworks/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,6 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .forRS256(apiAudience, issuer)
                 .configure(http)
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest()
                 .authenticated();
     }
